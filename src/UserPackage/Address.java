8336f86241c00b6aac56700347f8188954f6803f
Mrtin Rockinstone <59373264+maxxxdj@users.noreply.github.com>
2021-01-13 04:31:10 +0200

    Update Address.java

--- src/UserPackage/Address.java
+++ src/UserPackage/Address.java
@@ -7,4 +7,63 @@ public class Address {
     private String city;
     private String country;
     private String phone;
+
+    public Address(){
+    }
+
+    //Setters - Builder pattern;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public Address setStreet(String street) {
+        this.street = street;
+        return this;
+    }
+
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+
+    public Address setStreetNumber(int streetNumber) {
+        this.streetNumber = streetNumber;
+        return this;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    public Address setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+        return this;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Address setCity(String city) {
+        this.city = city;
+        return this;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public Address setCountry(String country) {
+        this.country = country;
+        return this;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public Address setPhone(String phone) {
+        this.phone = phone;
+        return this;
+    }
 }
